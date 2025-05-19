63b92c7d76356e60cde9160f8b54b7b3a6db3f91
MiguelCs03 <mangelcs2003@gmail.com>
2025-05-18 22:02:09 -0400

    Corrijo submódulo: agrego contenido real del frontend

---
+++ Frontend/aula-virtual/src/app/modules/auth/pages/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+
+  onSubmit() {
+    console.log('Usuario:', this.username);
+    console.log('Contraseña:', this.password);
+  }
+}

fca3fb11d30a07b1df0f73ce253164bdb3bf5a0b
mat2346 <javiersolizrueda1email@example.com>
2025-05-28 13:47:55 -0300

    agregando campo calificacion

--- Backend/actividad/models.py
+++ Backend/actividad/models.py
@@ -15,6 +15,7 @@ class Actividad(models.Model):
     dimension = models.ForeignKey(Dimension, on_delete=models.CASCADE, related_name='actividades')
     fecha = models.DateField(auto_now_add=True)  # Fecha de creación automática
     detalle_materia = models.ForeignKey(DetalleMateria, on_delete=models.CASCADE, related_name='actividades')
+    calificacion = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)  # Campo para la calificación
 
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre} - Calificación: {self.calificacion}"
